87f3707058ef64764f81f81f65485d26a4c9672f
Krzysztof Kozak <hithard5@gmail.com>
2022-06-15 17:50:17 +0200

    Add student card functions

--- index.js
+++ index.js
@@ -31,3 +31,54 @@ const students = [
     marks: [9, 8, 9, 7, 5],
   },
 ];
+
+const studentList = document.querySelector('[data-id="student-list"]');
+
+function createStudentCard(studentData) {
+  const listItem = document.createElement("li");
+  const { name, lastName, marks } = studentData;
+
+  const nameParagraph = document.createElement("p");
+  nameParagraph.textContent = `Name: ${name}`;
+
+  const lastNameParagraph = document.createElement("p");
+  lastNameParagraph.textContent = `Last name: ${lastName}`;
+
+  const visitedLectureParagraph = document.createElement("p");
+
+  const numberOfVisitedLectures = marks.reduce((total, currentValue) => {
+    if (currentValue) {
+      return total + 1;
+    }
+    return total;
+  }, 0);
+
+  visitedLectureParagraph.textContent = `Lectures visited: ${numberOfVisitedLectures}`;
+
+  const averageMarkParagraph = document.createElement("p");
+
+  const numberOfMarks = marks.length;
+
+  const sumOfMarks = marks.reduce((total, currentValue) => {
+    const value = currentValue ? currentValue : 0;
+    return total + value;
+  });
+
+  const averageMark = sumOfMarks / numberOfMarks;
+
+  averageMarkParagraph.textContent = `Average mark: ${averageMark}`;
+
+  listItem.appendChild(nameParagraph);
+  listItem.appendChild(lastNameParagraph);
+  listItem.appendChild(visitedLectureParagraph);
+  listItem.appendChild(averageMarkParagraph);
+
+  return listItem;
+}
+
+function createStudentCards(studentsData) {
+  return studentsData.map((student) => createStudentCard(student));
+}
+
+const studentCards = createStudentCards(students);
+studentCards.forEach((card) => studentList.appendChild(card));
